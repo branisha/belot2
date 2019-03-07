8da0672e0beba44f6b7c3765586871c53c739227
branisha <nikola.glavina@student.um.si>
2019-03-07 21:43:34 +0100

    Update

---
+++ README.md
@@ -0,0 +1,10 @@
+# belot2
+Sister project of belaapp.</br>
+Will merge when everything is finished and working.</br>
+</br>
+Still need to implement some features :sweat_smile::
+- [x] Automatic calculation of game
+- [ ] Add sessionStorage and localStorage
+- [ ] Add options panel
+- [ ] Make UI more user friendly
+- [ ] Alert when game is over
--- script.js
+++ script.js
@@ -24,33 +24,33 @@ class Igra {
     addGame(partija) {
         this.partije.push(partija);
     }
-    addGameToTable(index, partija){
+    addGameToTable(index, partija) {
         let rows = index + 1 + ".";
 
         let new_row = document.createElement("tr");
         let col_1 = document.createElement("td");
         let col_2 = document.createElement("td");
         let col_3 = document.createElement("td");
-    
+
         $(col_1).text(rows);
         $(col_2).text(partija.mi);
         $(col_3).text(partija.vi);
-    
+
         $(new_row).append(col_1);
         $(new_row).append(col_2);
         $(new_row).append(col_3);
-    
+
         $("#blok tbody").append(new_row);
     }
 
-    refreshList(){
+    refreshList() {
         $("#blok tbody tr").remove();
         let mi_ukupno = 0;
         let vi_ukupno = 0;
-        for(let i = 0; i < this.partije.length; i++){
-           this.addGameToTable(i, this.partije[i]);
-           mi_ukupno += this.partije[i].mi;
-           vi_ukupno += this.partije[i].vi;
+        for (let i = 0; i < this.partije.length; i++) {
+            this.addGameToTable(i, this.partije[i]);
+            mi_ukupno += this.partije[i].mi;
+            vi_ukupno += this.partije[i].vi;
         }
         $("#status_mi").text(mi_ukupno);
         $("#status_vi").text(vi_ukupno);
@@ -58,13 +58,55 @@ class Igra {
 }
 
 function clearModal() {
+    $("#modal_game").text("162");
     $("#mi_ukupno").val("");
     $("#vi_ukupno").val("");
     $("input[value='mi']").prop("checked", true);
     $("#zvanja").val("");
+    $("#modal_status").val("");
 }
-function closeModal(){
-        $(".modal").css("display", "none");
+function closeModal() {
+    $(".modal").css("display", "none");
+}
+
+
+function updateZvanje() {
+    let zvanje = parseInt($("#zvanja").val());
+    if ($("#zvanja").val().length < 1) zvanje = 0;
+    $("#modal_game").text(162 + zvanje);
+    return zvanje;
+}
+
+function updateGame(element) {
+
+
+    let mi_vrijednost = parseInt($("#mi_ukupno").val());
+    let vi_vrijednost = parseInt($("#vi_ukupno").val());
+    let zvanje = updateZvanje();
+
+    if ($("#mi_ukupno")[0] == element) {
+        $("#vi_ukupno").val(162 + zvanje - mi_vrijednost);
+        vi_vrijednost = parseInt($("#vi_ukupno").val());
+    } else if ($("#vi_ukupno")[0] == element) {
+        $("#mi_ukupno").val(162 + zvanje - vi_vrijednost);
+        mi_vrijednost = parseInt($("#mi_ukupno").val());
+    }
+
+    if ($("input[type='radio']:checked").val() == ("mi")) {
+        if ((162 + zvanje) / 2 > mi_vrijednost) {
+            $("#modal_status").text("Pisati će se puna igra pod \"VI\"");
+        } else {
+            $("#modal_status").text("");
+        }
+
+    } else {
+        console.log((162 + zvanje) / 2);
+        if ((162 + zvanje) / 2 > vi_vrijednost) {
+            $("#modal_status").text("Pisati će se puna igra pod \"MI\"");
+        } else {
+            $("#modal_status").text("");
+        }
+    }
 }
 
 $(document).ready(function () {
@@ -73,23 +115,67 @@ $(document).ready(function () {
 
 
     $("#modal_save").click(function () {
-        let mi = $("#mi_ukupno").val();
-        let vi = $("#vi_ukupno").val();
+        let mi = parseInt($("#mi_ukupno").val());
+        let vi = parseInt($("#vi_ukupno").val());
         let zvao = $("input[name='zvao']:checked").val();
-        let zvanja = $("#zvanja").val();
+        let zvanja = parseInt($("#zvanja").val());
 
-        if (mi.length < 1 || vi.length < 1 || zvanja.length < 1) {
+        if (Number.isNaN(mi) || Number.isNaN(vi)) {
             console.log("Error");
+            $("#modal_status").text("Vrijednosti nisu uredu");
+
         } else {
+
+            if (Number.isNaN(zvanja)) zvanja = 0;
+
+            if (zvao == "mi" && mi < (162 + zvanja) / 2) {
+                mi = 0;
+                vi = 162 + zvanja;
+            } else if (zvao == "vi" && vi < (162 + zvanja) / 2) {
+                vi = 0;
+                mi = 162 + zvanja;
+            }
 
             let partija = new Partija(mi, vi, zvao, zvanja);
 
             game.addGame(partija);
-            console.log(game);
             clearModal();
             game.refreshList();
             closeModal();
+            str = JSON.stringify(game);
+            console.log(game);
+            Object.create
+
         }
 
+    });
+
+    $("input[type='radio']").click(function () {
+        updateGame($("input[type='radio']:checked")[0]);
+    });
+
+    $("#zvanja").keyup(function () {
+
+        let zvanje = updateZvanje();
+
+        if($("input[type='radio']:checked").val() == "mi"){
+            let new_value = parseInt($("#vi_ukupno").val());
+            new_value = 162 + zvanje - new_value;
+            console.log(zvanje);
+            $("#mi_ukupno").val(new_value);
+
+        }else{
+            let new_value = parseInt($("#mi_ukupno").val());
+            new_value = 162 + zvanje - new_value;
+            console.log(zvanje);
+            $("#vi_ukupno").val(new_value);
+        }
+
+        updateGame($("input[type='radio']:checked")[0]);
+
+    });
+
+    $("#mi_ukupno, #vi_ukupno").keyup(function () {
+        updateGame(this);
     });
 });
